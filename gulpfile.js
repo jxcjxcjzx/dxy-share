e22c8823df7d71043fb63031a236713ee41ac0be
DIYgod <diy.d.god@gmail.com>
2016-04-06 11:15:03 +0800

    jquery and mobile support

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var uglify      = require('gulp-uglify');
 var rename      = require('gulp-rename');
 var babel       = require("gulp-babel");
 var sourcemaps  = require("gulp-sourcemaps");
+var prefix      = require('gulp-autoprefixer');
+var cssNano     = require('gulp-cssnano');
 var browserSync = require('browser-sync').create();
 
 // Launch the server
@@ -31,14 +33,27 @@ gulp.task('compressJS', function() {
         .pipe(browserSync.stream());
 });
 
+// Build css files
+gulp.task('compressCSS', function() {
+    gulp.src('src/*.css')
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(cssNano())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
 // Watch files for changes & recompile
 gulp.task('watch', function () {
-    gulp.watch(['src/*.js'], ['compressJS']);
+    gulp.watch(['src/*.js'], ['compressJS', 'compressCSS']);
+    gulp.watch(['src/*.css'], ['compressCSS']);
     gulp.watch('demo/*.html').on('change', browserSync.reload);
 });
 
 // compile the project, including move font, compress js and scss, also be used to test
-gulp.task('release', ['compressJS']);
+gulp.task('release', ['compressJS', 'compressCSS']);
 
 // Default task, running just `gulp` will move font, compress js and scss, launch server, watch files.
 gulp.task('default', ['release', 'browser-sync', 'watch']);
